6cec13dd6dc91d419f81bb7fb2bfb78466850834
Paul Johann Magnus Litschen <stu231377@mail.uni-kiel.de>
2026-08-11 14:11:00 +0200

    added login

--- data/utility/api_manager.py
+++ data/utility/api_manager.py
@@ -44,7 +44,7 @@ class ApiManager:
             self._password = options.pw
 
            
-            leagues = [Kickbase.LeagueData.league_data(d) for d in j["srvl"]]
+            leagues = [Kickbase.league_data(d) for d in j["srvl"]]
 
         elif response.status_code == 401:
             raise Exception()
