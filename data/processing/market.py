b7b669c0a5066bd7ad554eaaf79ad49ab8f0e70c
Paul Johann Magnus Litschen <stu231377@mail.uni-kiel.de>
2026-08-15 16:51:39 +0200

    fixed revenue and market

--- data/processing/market.py
+++ data/processing/market.py
@@ -17,12 +17,12 @@ def get_market_players():
     players = [player for player in manager.get(f"/leagues/{manager.leagueid}/market")["it"] if not player.get("u")]
 
     for player in tqdm(players, desc="Collecting players on market"):
-        real_player = manager.get(f'/leagues/{manager.leagueid}/players/{player["pi"]}')
-        expiration_time = (datetime.now(timezone('Europe/Berlin')) + timedelta(seconds=int(player.expiry)))
+        real_player = manager.get(f'/leagues/{manager.leagueid}/players/{player["i"]}')
+        expiration_time = (datetime.now(timezone('Europe/Berlin')) + timedelta(seconds=int(real_player["exs"])))
         result.append({'first_name': real_player['fn'],
                        'last_name': real_player['ln'],
                        'market_value': real_player['mv'],
-                       'expiration': real_player["exs"]/3600,
+                       'expiration': expiration_time,
                        'team_id': real_player['tid'],
                        'position': constants.POSITIONS[real_player['pos']],
                        'trend': real_player['tfhmvt']})
