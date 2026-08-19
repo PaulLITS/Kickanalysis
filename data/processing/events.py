488795bf55ed8870a584de4422fca1d9dafab96f
Paul Johann Magnus Litschen <stu231377@mail.uni-kiel.de>
2026-08-20 00:56:49 +0200

    added event counter

--- data/processing/events.py
+++ data/processing/events.py
@@ -23,8 +23,8 @@ def calculate_events_for_each_manager():
         
         
             
-        if manager.league_current_matchday(min) < len(result[user["n"]]):
-            for player in result[user["n"]][manager.league_current_matchday(min)-1]:
+        if manager.league_current_matchday(min) < len(result[user["i"]]):
+            for player in result[user["i"]][manager.league_current_matchday(min)-1]:
                 performance = manager.get(f"/leagues/{manager.leagueid}/players/{player}/performance")["it"][-1]["ph"][manager.league_current_matchday(min)-2]["k"]
                 
                 events = performance.get("k")
