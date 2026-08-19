2eacdff5fa12a65ea9d2480444c575a7a349ec8e
Paul Johann Magnus Litschen <stu231377@mail.uni-kiel.de>
2026-08-19 16:08:30 +0200

    throwing stuff at the wall

--- data/processing/turnovers.py
+++ data/processing/turnovers.py
@@ -16,7 +16,7 @@ def get_turnovers():
 
         for buy in manager.get_transfers_raw(user['i']):
             if parser.parse(buy['dt']) < manager.start:
-                break
+                continue
 
             transfer_type = 'buy' if buy['tty'] == 1 else 'sell'
 
