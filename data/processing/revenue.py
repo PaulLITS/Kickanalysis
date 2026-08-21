9099a74e268595480b744019cd1cb0d418cb3e0b
Paul Johann Magnus Litschen <stu231377@mail.uni-kiel.de>
2026-08-21 23:19:37 +0200

    changed too much shits gonnA crash

--- data/processing/revenue.py
+++ data/processing/revenue.py
@@ -109,13 +109,13 @@ def calculate_daily_budget():
        
 def calculate_daily_gain():
     dt = datetime.now()
-    
+    print("we are here")
     if os.path.exists("./data/gain.json"):
         with open("./data/gain.json", "r", encoding="utf-8") as f:
             gain_data = json.load(f)
     else:
         gain_data = {}
-        
+    print(str(dt.hour))    
     if dt.hour >= 22 and dt.hour < 24:
         print("we")
         current_day = datetime.now().strftime("%Y-%m-%d")
