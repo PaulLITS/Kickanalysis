8ed59b7094979a41cf8b2ffd182adf23915a4273
Paul Johann Magnus Litschen <stu231377@mail.uni-kiel.de>
2026-08-31 00:20:34 +0200

    Changed revenue and implement events

--- data/processing/events.py
+++ data/processing/events.py
@@ -34,7 +34,7 @@ def calculate_events_for_each_manager():
                     continue
                 
                 for x in events:
-                    events_data[user["n"]][x] += 1           
+                    events_data[user["n"]]["x"] += 1           
                   
     with open("./data/events.json", "w", encoding="utf-8") as f:
         json.dump(events_data, f, indent=2, ensure_ascii=False,)
