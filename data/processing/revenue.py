09905e3d48e5925627e925f325835358ba95d594
Paul Johann Magnus Litschen <stu231377@mail.uni-kiel.de>
2026-09-01 23:01:30 +0200

    change exP

--- data/processing/revenue.py
+++ data/processing/revenue.py
@@ -95,6 +95,8 @@ def calculate_daily_budget():
             for player in Response["it"]
         ) + 150000000  + 100000*(days_since(manager.start)-10) + 450000 + (dashboard["tp"]*1000) +  revenue_data[user["n"]][-1][1]
         
+        print(f"{sum(player.get("mvgl", 0) for player in Response["it"])}/+/{days_since(manager.start)}/+/{(dashboard["tp"]*1000)}/+/{revenue_data[user["n"]][-1][1]}")
+        
         # user should already exist in revenue_sum.json 
         if user["n"] not in Budget_data:
             Budget_data[user["n"]] = {}
